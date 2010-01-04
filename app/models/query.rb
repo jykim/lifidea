9ad30ba4526a101d93da8dc07a3d8ee76a3ec812
Jinyoung Kim <jykim@cowra.cs.umass.edu>
2010-01-04 17:25:52 -0500

    production bug fix for gavg

--- app/models/query.rb
+++ app/models/query.rb
@@ -6,7 +6,7 @@ class Query < ActiveRecord::Base
   named_scope :between, lambda{|start_at, end_at| {:conditions=>
     ["created_at >= ? and created_at < ?", start_at, end_at]}}
   
-  named_scope :valid, {:conditions=>["position > ? and position <= ?", 0, 10]}
+  named_scope :valid, {:conditions=>["position > ? and position <= ?", 0, 50]}
   
   def self.valid_queries(item_id)
     Query.valid.find_all_by_item_id(item_id)
