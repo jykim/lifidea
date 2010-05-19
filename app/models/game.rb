6319872aecb047bc2f8ab460307392d30b6a51c2
jykim <lifidea@gmail.com>
2010-05-19 09:50:20 -0400

    *Bug Fix *Announcement

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@
 class Game < ActiveRecord::Base
   belongs_to :user
   
-  named_scope :valid, {:conditions=>["score > ?", 0]}
+  named_scope :valid, {:conditions=>["score > ? and hidden_flag != 1", 0]}
   named_scope :displayable, {:conditions=>["user_id not in (1,23)"]}
 end
