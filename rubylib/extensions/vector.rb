c32adc904fcf35ea54d13624fd29f682d0011b2b
Jinyoung Kim <jykim@lifidea.local>
2011-05-04 06:25:01 -0400

    !

--- rubylib/extensions/vector.rb
+++ rubylib/extensions/vector.rb
@@ -1,13 +1,13 @@
 # Hash as sparse Vector
 module SparseVector
-  def normalize
+  def vsize
     Math.sqrt(values.inject(0){|sum,e| sum + e ** 2})
   end
   
   def product(other)
-    result = {}
+    result = 0.0
     each do |k,v|
-      result[k] = v * other[k] if v && other[k]
+      result += v * other[k] if v && other[k]
     end
     result
     #self.merge(other){|k,v1,v2|v1*v2}
@@ -15,6 +15,8 @@ module SparseVector
   
   def cosim(other)
     #debugger
-    self.product(other).sum{|k,v|v} / (self.normalize * other.normalize)
+    #p self
+    #p other
+    self.product(other) / (self.vsize * other.vsize)
   end
 end
